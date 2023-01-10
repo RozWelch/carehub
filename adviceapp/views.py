678005153b5272b4b238d866bdd0c5711b0c7a3b
RozWelch <rozwelch@gmail.com>
2023-01-10 19:06:50 +0000

    Add html basic files to template folder

--- adviceapp/views.py
+++ adviceapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Carearticle
 
-# Create your views here.
+
+"""Displays all articles in the browse articles page"""
+class CarearticleList(generic.ListView):
+    model = Carearticle
+    queryset = Carearticle.objects.filter(approved_status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 8
