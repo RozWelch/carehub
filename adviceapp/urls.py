0120a4f1785a62ac0e901d370a818c3bc6940c21
RozWelch <rozwelch@gmail.com>
2023-01-11 14:08:17 +0000

    Add post details view

--- adviceapp/urls.py
+++ adviceapp/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.CarearticleList.as_view(), name='home')
+    path('', views.CarearticleList.as_view(), name='home'),
+    path('<slug:slug>/', views.CarearticleDetail.as_view(), name='post_detail'),
 ]
--- adviceapp/views.py
+++ adviceapp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Carearticle
 
 
@@ -9,3 +9,24 @@ class CarearticleList(generic.ListView):
     queryset = Carearticle.objects.filter(approved_status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 8
+
+"""Displays single articles view with all content and comments"""
+class CarearticleDetail(View):
+
+    def get(self, request, slug, *arges, **kwargs):
+        queryset = Carearticle.objects.filter(approved_status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('-created_on')
+        helpful_ticks = False
+        if post.helpful_ticks.filter(id=self.request.user.id).exists():
+            helpful_ticks = True
+
+        return render(
+            request, 
+            "post_details.html",
+            {
+                "post": post,
+                "comments": comments,
+                "helpful_ticks": helpful_ticks
+            },
+        )
