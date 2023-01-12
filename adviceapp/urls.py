a8d4ff1fb785a09ec8d8ee9ee6ac42c34073fae1
RozWelch <rozwelch@gmail.com>
2023-01-12 21:23:28 +0000

    Add view for helpful ticks

--- adviceapp/urls.py
+++ adviceapp/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.CarearticleList.as_view(), name='home'),
     path('<slug:slug>/', views.CarearticleDetail.as_view(), name='post_detail'),
+    path('like/<slug:slug>', views.CarearticleTick.as_view(), name='post_tick'),
 ]
